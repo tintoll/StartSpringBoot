5f45944f53be104ccf84adae2ad01f0e91d60639
tintoll <belselios@gmail.com>
2019-06-26 23:48:16 +0900

    chap 04-2 까지 완료

--- board/src/main/java/board/controller/BoardController.java
+++ board/src/main/java/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import board.dto.BoardDto;
+import board.service.BoardService;
 
 @Controller
 public class BoardController {
